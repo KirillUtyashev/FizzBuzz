a09798720ba8fb7f27b6e436bcd8ca160a466c06
kirillutyashev <kirillut04@gmail.com>
2024-09-09 09:56:25 -0400

    Created Main file

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,20 +4,20 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
 
         while (i < 100) {
 
             // Find out which numbers divide i.
-            i = getI(i);
+            getI(i);
+
+            i += 1;
         }
     }
 
-    private static int getI(int i) {
+    static int getI(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
-
-        i += 1;
 
         // Print our appropriate result.
         if (divisibleBy3 && divisibleBy5) {
